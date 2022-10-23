0cd221db5c58fcbf68184f007888cec4a972555c
AnnaYeremenko <a.zaliubovska@gmail.com>
2022-10-23 15:11:26 +0300

    hw 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+const base_url = 'https://restcountries.com/v3.1/name/';
+
+export const fetchCountries = name => {
+  return fetch(
+    `${base_url}${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
